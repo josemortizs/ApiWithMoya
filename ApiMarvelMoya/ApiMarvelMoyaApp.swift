949bbc8ac9859aac71fca45ef6a4db41461eaede
José Manuel Ortiz Sánchez <39153780+josemortizs@users.noreply.github.com>
2021-02-22 16:05:23 +0100

    Initial Commit

---
+++ ApiMarvelMoya/ApiMarvelMoyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiMarvelMoyaApp.swift
+//  ApiMarvelMoya
+//
+//  Created by Jose Manuel Ortiz Sanchez on 22/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ApiMarvelMoyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiMarvelMoya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApiMarvelMoya
+//
+//  Created by Jose Manuel Ortiz Sanchez on 22/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
